f5759fe6c36ebce7d20ca0912b13055337971631
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2020-06-22 21:59:30 +0530

    Remove extra tokens for now - keep initial implementation limited to specific subset of regex syntax

--- lexer.c
+++ lexer.c
@@ -38,91 +38,9 @@ next_token(lexer_t *l)
     if (t == NULL)
         err(EXIT_FAILURE, "malloc failed");
     switch (l->ch) {
-    case '\\':
-        if (l->input[l->read_offset] == 'd') {
-            t->type = DIGIT_CLASS;
-            t->literal = strdup("\\d");
-        } else if (l->input[l->read_offset] == 'D') {
-            t->type = NON_DIGIT_CLASS;
-            t->literal = strdup("\\D");
-        } else if (l->input[l->read_offset] == 'w') {
-            t->type = ALPHANUMERIC_CLASS;
-            t->literal = strdup("\\w");
-        } else if (l->input[l->read_offset] == 'W') {
-            t->type = NON_ALPHANUMERIC_CLASS;
-            t->literal = strdup("\\W");
-        } else if (l->input[l->read_offset] == 's') {
-            t->type = WHITE_SPACE_CLASS;
-            t->literal = strdup("\\s");
-        } else if (l->input[l->read_offset] == 'S') {
-            t->type = NON_WHITE_SPACE_CLASS;
-            t->literal = strdup("\\S");
-        } else if (l->input[l->read_offset] == '\\') {
-            t->type = CHAR;
-            t->literal = strdup("\\");
-        } else if (l->input[l->read_offset] == '.') {
-            t->type = CHAR;
-            t->literal = strdup(".");
-        } else if (l->input[l->read_offset] == '^') {
-            t->type = CHAR;
-            t->literal = strdup("^");
-        } else if (l->input[l->read_offset] == '$') {
-            t->type = CHAR;
-            t->literal = strdup("$");
-        } else if (l->input[l->read_offset] == '+') {
-            t->type = CHAR;
-            t->literal = strdup("+");
-        } else if (l->input[l->read_offset] == '?') {
-            t->type = CHAR;
-            t->literal = strdup("?");
-        } else if (l->input[l->read_offset] == '[') {
-            t->type = CHAR;
-            t->literal = strdup("[");
-        } else if (l->input[l->read_offset] == ']') {
-            t->type = CHAR;
-            t->literal = strdup("]");
-        } else if (l->input[l->read_offset] == '{') {
-            t->type = CHAR;
-            t->literal = strdup("{");
-        } else if (l->input[l->read_offset] == '}') {
-            t->type = CHAR;
-            t->literal = strdup("}");
-        } else if (l->input[l->read_offset] == '(') {
-            t->type = CHAR;
-            t->literal = strdup("(");
-        } else if (l->input[l->read_offset] = ')') {
-            t->type = CHAR;
-            t->literal = strdup(")");
-        } else if (l->input[l->read_offset] == '|') {
-            t->type = CHAR;
-            t->literal = strdup("|");
-        } else if (l->input[l->read_offset] == '*') {
-            t->type = CHAR;
-            t->literal = strdup("*");
-        } else if (l->input[l->read_offset] == '-') {
-            t->type = CHAR;
-            t->literal = strdup("-");
-        } else {
-            t->type = ILLEGAL;
-        }
-        if (t->type != ILLEGAL) {
-            read_char(l);
-            read_char(l);
-        }
-        break;
     case '.':
         t->type = DOT;
         t->literal = strdup(".");
-        read_char(l);
-        break;
-    case '^':
-        t->type = CARET;
-        t->literal = strdup("^");
-        read_char(l);
-        break;
-    case '$':
-        t->type = DOLLAR;
-        t->literal = strdup("$");
         read_char(l);
         break;
     case '+':
@@ -133,26 +51,6 @@ next_token(lexer_t *l)
     case '?':
         t->type = QUESTION;
         t->literal = strdup("?");
-        read_char(l);
-        break;
-    case '[':
-        t->type = LBRACKET;
-        t->literal = strdup("[");
-        read_char(l);
-        break;
-    case ']':
-        t->type = RBRACKET;
-        t->literal = strdup("]");
-        read_char(l);
-        break;
-    case '{':
-        t->type = LBRACE;
-        t->literal = strdup("{");
-        read_char(l);
-        break;
-    case '}':
-        t->type = RBRACE;
-        t->literal = strdup("}");
         read_char(l);
         break;
     case '(':
@@ -175,10 +73,9 @@ next_token(lexer_t *l)
         t->literal = strdup("|");
         read_char(l);
         break;
-    case '-':
-        t->type = HYPHEN;
-        t->literal = strdup("-");
-        read_char(l);
+    case 0:
+        t->type = END_OF_FILE;
+        t->literal = "";
         break;
     default:
         t->type = CHAR;
--- token.c
+++ token.c
@@ -9,58 +9,23 @@ get_token_type(const char *tok_literal)
     if (tok_literal[0] == 0)
         return END_OF_FILE;
 
-    if (tok_literal[1] == 0) {
-        switch (tok_literal[0]) {
-        case '.':
-            return DOT;
-        case '^':
-            return CARET;
-        case '$':
-            return DOLLAR;
-        case '+':
-            return PLUS;
-        case '?':
-            return QUESTION;
-        case '{':
-            return LBRACE;
-        case '}':
-            return RBRACE;
-        case '[':
-            return LBRACKET;
-        case ']':
-            return RBRACKET;
-        case '(':
-            return LPAREN;
-        case ')':
-            return RPAREN;
-        case '|':
-            return PIPE;
-        case '*':
-            return STAR;
-        case '-':
-            return HYPHEN;
-        default:
-            return CHAR;
-        }
-    }
-
-    if (tok_literal[2] == 0 && tok_literal[0] == '\\') {
-        switch (tok_literal[1]) {
-            case 'd':
-                return DIGIT_CLASS;
-            case 'D':
-                return NON_DIGIT_CLASS;
-            case 's':
-                return WHITE_SPACE_CLASS;
-            case 'S':
-                return NON_WHITE_SPACE_CLASS;
-            case 'w':
-                return ALPHANUMERIC_CLASS;
-            case 'W':
-                return NON_ALPHANUMERIC_CLASS;
-            default:
-                return CHAR;
-        }
+    switch (tok_literal[0]) {
+    case '.':
+        return DOT;
+    case '+':
+        return PLUS;
+    case '?':
+        return QUESTION;
+    case '(':
+        return LPAREN;
+    case ')':
+        return RPAREN;
+    case '|':
+        return PIPE;
+    case '*':
+        return STAR;
+    default:
+        return CHAR;
     }
 
     return CHAR;
